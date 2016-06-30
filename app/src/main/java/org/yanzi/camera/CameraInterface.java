551fb4d9b491eb02d1d2794810ca2c10bbbe796d
yuntongzhong <847654033@qq.com>
2016-06-30 17:37:38 +0800

    。

--- app/src/main/java/org/yanzi/camera/CameraInterface.java
+++ app/src/main/java/org/yanzi/camera/CameraInterface.java
@@ -23,234 +23,237 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
-public class CameraInterface implements Camera.PreviewCallback{
-	private static final String TAG = "yanzi";
-	private Camera mCamera;
-	private Camera.Parameters mParams;
-	private boolean isPreviewing = false;
-	private float mPreviwRate = -1f;
-	private static CameraInterface mCameraInterface;
-	private CropInfo mCropInfo;
-	private ResultCallback resultCallback;
+public class CameraInterface implements Camera.PreviewCallback {
+    private static final String TAG = "yanzi";
+    private Camera mCamera;
+    private Camera.Parameters mParams;
+    private boolean isPreviewing = false;
+    private float mPreviwRate = -1f;
+    private static CameraInterface mCameraInterface;
+    private CropInfo mCropInfo;
+    private ResultCallback resultCallback;
 
 
+    public interface CamOpenOverCallback {
+        void cameraHasOpened();
+    }
 
-	public interface CamOpenOverCallback{
-		public void cameraHasOpened();
-	}
-	public interface ResultCallback{
-		public void call(String result);
-	}
+    public interface ResultCallback {
+        void call(String result);
+    }
 
-	private CameraInterface(){
+    private CameraInterface() {
 
-	}
-	public static synchronized CameraInterface getInstance(){
-		if(mCameraInterface == null){
-			mCameraInterface = new CameraInterface();
-		}
-		return mCameraInterface;
-	}
-	public void setResultCallBack(ResultCallback callBack){
-		if(callBack!=null){
-			resultCallback=callBack;
-		}
+    }
 
-	}
+    public static synchronized CameraInterface getInstance() {
+        if (mCameraInterface == null) {
+            mCameraInterface = new CameraInterface();
+        }
+        return mCameraInterface;
+    }
 
-	/**打开Camera
-	 * @param callback
-	 */
-	public void doOpenCamera(CropInfo cropInfo,CamOpenOverCallback callback){
-		doStopCamera();
-		mCropInfo=cropInfo;
-		Log.i(TAG, "Camera open....");
-		mCamera = Camera.open();
-		Log.i(TAG, "Camera open over....");
-		if(null!=callback){
-			callback.cameraHasOpened();
-		}
+    public void setResultCallBack(ResultCallback callBack) {
+        if (callBack != null) {
+            resultCallback = callBack;
+        }
+    }
 
-	}
-	/**开启预览
-	 * @param holder
-	 * @param previewRate
-	 */
-	public void doStartPreview(SurfaceHolder holder, float previewRate){
-		Log.i(TAG, "doStartPreview...");
-		if(isPreviewing){
-			mCamera.stopPreview();
-			return;
-		}
-		if(mCamera != null){
-			mParams = mCamera.getParameters();
-			mParams.setPictureFormat(PixelFormat.JPEG);//设置拍照后存储的图片格式
-			CamParaUtil.getInstance().printSupportPictureSize(mParams);
-			CamParaUtil.getInstance().printSupportPreviewSize(mParams);
-			//设置PreviewSize和PictureSize
-			Size pictureSize = CamParaUtil.getInstance().getPropPictureSize(
-					mParams.getSupportedPictureSizes(),previewRate, 800);
-			mParams.setPictureSize(pictureSize.width, pictureSize.height);
+    /**
+     * 打开Camera
+     *
+     * @param callback
+     */
+    public void doOpenCamera(CropInfo cropInfo, CamOpenOverCallback callback) {
+        doStopCamera();
+        mCropInfo = cropInfo;
+        Log.i(TAG, "Camera open....");
+        mCamera = Camera.open();
+        Log.i(TAG, "Camera open over....");
+        if (null != callback) {
+            callback.cameraHasOpened();
+        }
 
-			Size previewSize = CamParaUtil.getInstance().getPropPreviewSize(
-					mParams.getSupportedPreviewSizes(), previewRate, 800);
-			mParams.setPreviewSize(previewSize.width, previewSize.height);
+    }
 
-			mCamera.setDisplayOrientation(90);
+    /**
+     * 开启预览
+     *
+     * @param holder
+     * @param previewRate
+     */
+    public void doStartPreview(SurfaceHolder holder, float previewRate) {
+        Log.i(TAG, "doStartPreview...");
+        if (isPreviewing) {
+            mCamera.stopPreview();
+            return;
+        }
+        if (mCamera != null) {
+            mParams = mCamera.getParameters();
+            mParams.setPictureFormat(PixelFormat.JPEG);//设置拍照后存储的图片格式
+            CamParaUtil.getInstance().printSupportPictureSize(mParams);
+            CamParaUtil.getInstance().printSupportPreviewSize(mParams);
+            //设置PreviewSize和PictureSize
+            Size pictureSize = CamParaUtil.getInstance().getPropPictureSize(
+                    mParams.getSupportedPictureSizes(), previewRate, 800);
+            mParams.setPictureSize(pictureSize.width, pictureSize.height);
 
-			CamParaUtil.getInstance().printSupportFocusMode(mParams);
-			List<String> focusModes = mParams.getSupportedFocusModes();
-			if(focusModes.contains("continuous-video")){
-				mParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
-			}
-			mCamera.setParameters(mParams);
+            Size previewSize = CamParaUtil.getInstance().getPropPreviewSize(
+                    mParams.getSupportedPreviewSizes(), previewRate, 800);
+            mParams.setPreviewSize(previewSize.width, previewSize.height);
 
-			try {
-				mCamera.setPreviewDisplay(holder);
-				mCamera.startPreview();//开启预览
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            mCamera.setDisplayOrientation(90);
 
-			isPreviewing = true;
-			mPreviwRate = previewRate;
-			mCamera.setPreviewCallback(this);
-			mParams = mCamera.getParameters(); //重新get一次
-			Log.i(TAG, "最终设置:PreviewSize--With = " + mParams.getPreviewSize().width
-					+ "Height = " + mParams.getPreviewSize().height);
-			Log.i(TAG, "最终设置:PictureSize--With = " + mParams.getPictureSize().width
-					+ "Height = " + mParams.getPictureSize().height);
-		}
-	}
-	/**
-	 * 停止预览，释放Camera
-	 */
-	public void doStopCamera(){
-		if(null != mCamera)
-		{
-			mCamera.setPreviewCallback(null);
-			mCamera.stopPreview();
-			isPreviewing = false;
-			mPreviwRate = -1f;
-			mCamera.release();
-			mCamera = null;
-		}
-	}
-	/**
-	 * 拍照
-	 */
-	public void doTakePicture(){
-		if(isPreviewing && (mCamera != null)){
-			mCamera.takePicture(mShutterCallback, null, mJpegPictureCallback);
-		}
-	}
+            CamParaUtil.getInstance().printSupportFocusMode(mParams);
+            List<String> focusModes = mParams.getSupportedFocusModes();
+            if (focusModes.contains("continuous-video")) {
+                mParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+            }
+            mCamera.setParameters(mParams);
 
-	/*为了实现拍照的快门声音及拍照保存照片需要下面三个回调变量*/
-	ShutterCallback mShutterCallback = new ShutterCallback()
-			//快门按下的回调，在这里我们可以设置类似播放“咔嚓”声之类的操作。默认的就是咔嚓。
-	{
-		public void onShutter() {
-			// TODO Auto-generated method stub
-			Log.i(TAG, "myShutterCallback:onShutter...");
-		}
-	};
-	PictureCallback mRawCallback = new PictureCallback()
-			// 拍摄的未压缩原数据的回调,可以为null
-	{
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();//开启预览
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-		public void onPictureTaken(byte[] data, Camera camera) {
-			// TODO Auto-generated method stub
-			Log.i(TAG, "myRawCallback:onPictureTaken...");
+            isPreviewing = true;
+            mPreviwRate = previewRate;
+            mCamera.setPreviewCallback(this);
+            mParams = mCamera.getParameters(); //重新get一次
+            Log.i(TAG, "最终设置:PreviewSize--With = " + mParams.getPreviewSize().width
+                    + "Height = " + mParams.getPreviewSize().height);
+            Log.i(TAG, "最终设置:PictureSize--With = " + mParams.getPictureSize().width
+                    + "Height = " + mParams.getPictureSize().height);
+        }
+    }
 
-		}
-	};
-	PictureCallback mJpegPictureCallback = new PictureCallback()
-			//对jpeg图像数据的回调,最重要的一个回调
-	{
-		public void onPictureTaken(byte[] data, Camera camera) {
-			// TODO Auto-generated method stub
-			Log.i(TAG, "myJpegCallback:onPictureTaken...");
-			Bitmap b = null;
-			if(null != data){
-				b = BitmapFactory.decodeByteArray(data, 0, data.length);//data是字节数据，将其解析成位图
-				mCamera.stopPreview();
-				isPreviewing = false;
-			}
-			//保存图片到sdcard
-			if(null != b)
-			{
-				//设置FOCUS_MODE_CONTINUOUS_VIDEO)之后，myParam.set("rotation", 90)失效。
-				//图片竟然不能旋转了，故这里要旋转下
-				Bitmap rotaBitmap = ImageUtil.getRotateBitmap(b, 90.0f);
-				FileUtil.saveBitmap(rotaBitmap);
-			}
-			//再次进入预览
-			mCamera.startPreview();
-			isPreviewing = true;
-		}
-	};
-	FaceTask mFaceTask=null;
+    /**
+     * 停止预览，释放Camera
+     */
+    public void doStopCamera() {
+        if (null != mCamera) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            isPreviewing = false;
+            mPreviwRate = -1f;
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 
-	@Override
-	public void onPreviewFrame(byte[] bytes, Camera camera) {
-		if(null != mFaceTask){
-			switch(mFaceTask.getStatus()){
-				case RUNNING:
-					return;
-				case PENDING:
-					mFaceTask.cancel(false);
-					break;
-			}
-		}
-		mFaceTask = new FaceTask(bytes,mCamera.getParameters().getPreviewSize());
-		mFaceTask.execute();
+    /**
+     * 拍照
+     */
+    public void doTakePicture() {
+        if (isPreviewing && (mCamera != null)) {
+            mCamera.takePicture(mShutterCallback, null, mJpegPictureCallback);
+        }
+    }
 
-	}
+    /*为了实现拍照的快门声音及拍照保存照片需要下面三个回调变量*/
+    ShutterCallback mShutterCallback = new ShutterCallback()
+            //快门按下的回调，在这里我们可以设置类似播放“咔嚓”声之类的操作。默认的就是咔嚓。
+    {
+        public void onShutter() {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "myShutterCallback:onShutter...");
+        }
+    };
+    PictureCallback mRawCallback = new PictureCallback()
+            // 拍摄的未压缩原数据的回调,可以为null
+    {
 
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "myRawCallback:onPictureTaken...");
 
-	private class FaceTask extends AsyncTask<Void, Void, Void> {
+        }
+    };
+    PictureCallback mJpegPictureCallback = new PictureCallback()
+            //对jpeg图像数据的回调,最重要的一个回调
+    {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "myJpegCallback:onPictureTaken...");
+            Bitmap b = null;
+            if (null != data) {
+                b = BitmapFactory.decodeByteArray(data, 0, data.length);//data是字节数据，将其解析成位图
+                mCamera.stopPreview();
+                isPreviewing = false;
+            }
+            //保存图片到sdcard
+            if (null != b) {
+                //设置FOCUS_MODE_CONTINUOUS_VIDEO)之后，myParam.set("rotation", 90)失效。
+                //图片竟然不能旋转了，故这里要旋转下
+                Bitmap rotaBitmap = ImageUtil.getRotateBitmap(b, 90.0f);
+                FileUtil.saveBitmap(rotaBitmap);
+            }
+            //再次进入预览
+            mCamera.startPreview();
+            isPreviewing = true;
+        }
+    };
+    FaceTask mFaceTask = null;
 
-		private byte[] mData;
-		Size mSize;
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        if (null != mFaceTask) {
+            switch (mFaceTask.getStatus()) {
+                case RUNNING:
+                    return;
+                case PENDING:
+                    mFaceTask.cancel(false);
+                    break;
+            }
+        }
+        mFaceTask = new FaceTask(bytes, mCamera.getParameters().getPreviewSize());
+        mFaceTask.execute();
+    }
 
-		//构造函数
-		FaceTask(byte[] data,Size size){
-			this.mData = data;
-			this.mSize=size;
-		}
+    private class FaceTask extends AsyncTask<Void, Void, Void> {
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			// TODO Auto-generated method stub
-			//Size size = myCamera.getParameters().getPreviewSize(); //获取预览大小
-			final int w = mSize.width;  //宽度
-			final int h = mSize.height;
-			final YuvImage image = new YuvImage(mData, ImageFormat.NV21, w, h, null);
-			ByteArrayOutputStream os = new ByteArrayOutputStream(mData.length);
-			if(!image.compressToJpeg(new Rect(0, 0, w, h), 100, os)){
-				return null;
-			}
-			byte[] tmp = os.toByteArray();
-			Bitmap bmp = BitmapFactory.decodeByteArray(tmp, 0,tmp.length);
-			if(null!=bmp&&null!=mCropInfo){
-				Bitmap rotaBitmap = ImageUtil.getRotateBitmap(bmp, 90.0f);
+        private byte[] mData;
+        Size mSize;
 
-				Log.e("rotaBitmap","width:"+rotaBitmap.getWidth()+",height:"+rotaBitmap.getHeight());
-				RealityInfo realityInfo=new RealityInfo(mCropInfo,rotaBitmap.getWidth(),rotaBitmap.getHeight());
-				Bitmap endBitmap=Bitmap.createBitmap(rotaBitmap,realityInfo.realityX,realityInfo.realityY,realityInfo.realityWidth,realityInfo.realityHeigh);
+        //构造函数
+        FaceTask(byte[] data, Size size) {
+            this.mData = data;
+            this.mSize = size;
+        }
 
-				//FileUtil.savePreviewBitmap(endBitmap);
+        @Override
+        protected Void doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+            //Size size = myCamera.getParameters().getPreviewSize(); //获取预览大小
+            final int w = mSize.width;  //宽度
+            final int h = mSize.height;
+            final YuvImage image = new YuvImage(mData, ImageFormat.NV21, w, h, null);
+            ByteArrayOutputStream os = new ByteArrayOutputStream(mData.length);
+            if (!image.compressToJpeg(new Rect(0, 0, w, h), 100, os)) {
+                return null;
+            }
+            byte[] tmp = os.toByteArray();
+            Bitmap bmp = BitmapFactory.decodeByteArray(tmp, 0, tmp.length);
+            if (null != bmp && null != mCropInfo) {
+                Bitmap rotaBitmap = ImageUtil.getRotateBitmap(bmp, 90.0f);
 
-				String result =RecognitionUtils.getInstance().startDecodeThread(endBitmap);
-				Log.e("result",result);
-				if(!"-1".equals(result)&&resultCallback!=null){
-					resultCallback.call(result);
-				}
-			}
-			Log.e("bitmap Width",""+bmp.getWidth());
-			return null;
-		}
-	}
+                Log.e("rotaBitmap", "width:" + rotaBitmap.getWidth() + ",height:" + rotaBitmap.getHeight());
+                RealityInfo realityInfo = new RealityInfo(mCropInfo, rotaBitmap.getWidth(), rotaBitmap.getHeight());
+                Bitmap endBitmap = Bitmap.createBitmap(rotaBitmap, realityInfo.realityX, realityInfo.realityY, realityInfo.realityWidth, realityInfo.realityHeigh);
+
+                //FileUtil.savePreviewBitmap(endBitmap);
+
+                String result = RecognitionUtils.getInstance().startDecodeThread(endBitmap);
+                Log.e("result", result);
+                if (!"-1".equals(result) && resultCallback != null) {
+                    resultCallback.call(result);
+                }
+            }
+            Log.e("bitmap Width", "" + bmp.getWidth());
+            return null;
+        }
+    }
 
 }
--- app/src/main/java/org/yanzi/util/RecognitionUtils.java
+++ app/src/main/java/org/yanzi/util/RecognitionUtils.java
@@ -1,19 +1,10 @@
 package org.yanzi.util;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Environment;
-import android.os.Message;
-import android.text.TextUtils;
 
 import com.tianruiworkroomocr.Native;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
